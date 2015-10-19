4c96200d25e86113e330c8c9e103049c0ee92144
Shanna Chau <shannachau@gmail.com>
2015-10-18 21:59:13 -0700

    Capitalizing Servo

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,24 +4,24 @@ $(function(){
   function keyPressOrClick(servo, keyCode, element){
     $(document).on('keypress', function(event){
       if (event.keyCode == keyCode){
-        socket.emit(servo, 'this is a test');
+        socket.emit('moveServo', servo);
       }
       console.log('Moving ' + servo);
     });
 
     $(element).on('click', function(){
-      socket.emit(servo, 'this is a test');
+      socket.emit('moveServo', servo);
       console.log('Moving ' + servo);
     })
   }
 
-  keyPressOrClick('servo2', 49, '.a');
-  keyPressOrClick('servo3', 50, '.s');
-  keyPressOrClick('servo4', 51, '.d');
-  keyPressOrClick('servo5', 52, '.f');
-  keyPressOrClick('servo6', 53, '.g');
-  keyPressOrClick('servo8', 54, '.h');
-  keyPressOrClick('servo9', 55, '.j');
-  keyPressOrClick('servo10', 56, '.k');
+  keyPressOrClick('Servo2', 49, '.a');
+  keyPressOrClick('Servo3', 50, '.s');
+  keyPressOrClick('Servo4', 51, '.d');
+  keyPressOrClick('Servo5', 52, '.f');
+  keyPressOrClick('Servo6', 53, '.g');
+  keyPressOrClick('Servo8', 54, '.h');
+  keyPressOrClick('Servo9', 55, '.j');
+  keyPressOrClick('Servo10', 56, '.k');
 
 });
