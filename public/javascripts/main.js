238fc8ecc9cd88c9eaefe7494faeb4ebcb3b28d7
Shanna Chau <shannachau@gmail.com>
2015-10-20 09:45:35 -0700

    Adding comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,8 @@
 $(function(){
+  // connect to server
   var socket = io.connect('http://localhost:8081');
 
+  // emits an event to server if key is clicked or triggered by keypress
   function keyPressOrClick(servo, keyCode, element){
     $(document).on('keypress', function(event){
       if (event.keyCode == keyCode){
@@ -17,6 +19,7 @@ $(function(){
     })
   }
 
+  // animation for keypress or click
   function expandAnimation(element){
     $(element).addClass('expand');
     $(element).removeClass('lighten-1');
@@ -34,5 +37,33 @@ $(function(){
   keyPressOrClick('Servo8', 54, '.h');
   keyPressOrClick('Servo9', 55, '.j');
   keyPressOrClick('Servo10', 56, '.k');
+
+
+
+  // mapping integers to keyPressorClick function to easily program songs
+  var numToKey = {};
+  numToKey[1] = keyPressOrClick.bind(numToKey, 'Servo2', 49, '.a');
+  numToKey[2] = keyPressOrClick.bind(numToKey, 'Servo3', 50, '.s');
+  numToKey[3] = keyPressOrClick.bind(numToKey, 'Servo4', 51, '.d');
+  numToKey[4] = keyPressOrClick.bind(numToKey, 'Servo5', 52, '.f');
+  numToKey[5] = keyPressOrClick.bind(numToKey, 'Servo6', 53, '.g');
+  numToKey[6] = keyPressOrClick.bind(numToKey, 'Servo8', 54, '.h');
+  numToKey[7] = keyPressOrClick.bind(numToKey, 'Servo9', 55, '.j');
+  numToKey[8] = keyPressOrClick.bind(numToKey, 'Servo10', 56, '.k');
+
+  var maryHadLamb = [3,2,1,2,3,3,3,2,2,2,3,5,5,3,2,1,2,3,3,3,3,2,2,3,2,1];
+
+  var song = maryHadLamb;
+
+  // setting 1000 ms delay while shifting through array
+  var loopSong = setInterval(function(){
+    numToKey[song.shift()]();
+
+    // stop the loop once you have looped through entire array
+    if (song.length == 0){
+      clearInterval(loopSong);
+      // song = maryHadLamb;
+    }
+  }, 1000);
 
 });
