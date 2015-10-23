fc366227353a801befbf70fc22c7cfaa23aec663
Shanna Chau <shannachau@gmail.com>
2015-10-22 17:48:26 -0700

    Changing name of key classes from letters to nums 1-8

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,25 +29,25 @@ $(function(){
     });
   }
 
-  keyPressOrClick('Servo2', 49, '.a');
-  keyPressOrClick('Servo3', 50, '.s');
-  keyPressOrClick('Servo4', 51, '.d');
-  keyPressOrClick('Servo5', 52, '.f');
-  keyPressOrClick('Servo6', 53, '.g');
-  keyPressOrClick('Servo8', 54, '.h');
-  keyPressOrClick('Servo9', 55, '.j');
-  keyPressOrClick('Servo10', 56, '.k');
+  keyPressOrClick('Servo2', 49, '.1');
+  keyPressOrClick('Servo3', 50, '.2');
+  keyPressOrClick('Servo4', 51, '.3');
+  keyPressOrClick('Servo5', 52, '.4');
+  keyPressOrClick('Servo6', 53, '.5');
+  keyPressOrClick('Servo8', 54, '.6');
+  keyPressOrClick('Servo9', 55, '.7');
+  keyPressOrClick('Servo10', 56, '.8');
 
   // mapping integers to keyPressorClick function to easily program songs
   var numToKey = {};
-  numToKey[1] = keyPressOrClick.bind(numToKey, 'Servo2', 49, '.a');
-  numToKey[2] = keyPressOrClick.bind(numToKey, 'Servo3', 50, '.s');
-  numToKey[3] = keyPressOrClick.bind(numToKey, 'Servo4', 51, '.d');
-  numToKey[4] = keyPressOrClick.bind(numToKey, 'Servo5', 52, '.f');
-  numToKey[5] = keyPressOrClick.bind(numToKey, 'Servo6', 53, '.g');
-  numToKey[6] = keyPressOrClick.bind(numToKey, 'Servo8', 54, '.h');
-  numToKey[7] = keyPressOrClick.bind(numToKey, 'Servo9', 55, '.j');
-  numToKey[8] = keyPressOrClick.bind(numToKey, 'Servo10', 56, '.k');
+  numToKey[1] = keyPressOrClick.bind(numToKey, 'Servo2', 49, '.1');
+  numToKey[2] = keyPressOrClick.bind(numToKey, 'Servo3', 50, '.2');
+  numToKey[3] = keyPressOrClick.bind(numToKey, 'Servo4', 51, '.3');
+  numToKey[4] = keyPressOrClick.bind(numToKey, 'Servo5', 52, '.4');
+  numToKey[5] = keyPressOrClick.bind(numToKey, 'Servo6', 53, '.5');
+  numToKey[6] = keyPressOrClick.bind(numToKey, 'Servo8', 54, '.6');
+  numToKey[7] = keyPressOrClick.bind(numToKey, 'Servo9', 55, '.7');
+  numToKey[8] = keyPressOrClick.bind(numToKey, 'Servo10', 56, '.8');
 
   var maryHadLamb = [3,2,1,2,3,3,3,2,2,2,3,5,5,3,2,1,2,3,3,3,3,2,2,3,2,1];
 
