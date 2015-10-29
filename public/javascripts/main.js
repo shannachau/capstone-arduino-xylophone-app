b854bcde94e2b5a3a2f76ea0d262846fdcf4731d
Shanna Chau <shannachau@gmail.com>
2015-10-28 21:34:17 -0700

    using setTimeout instead of setInterval to loop through songs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -67,40 +67,39 @@ $(function(){
   numToKey[7] = moveServo.bind(numToKey, 'Servo9');
   numToKey[8] = moveServo.bind(numToKey, 'Servo10');
 
+
+  // plays a song that's formatted as an array of integers with delay
+  function playSong(song, speed){
+    for(var i = 0; i < song.length; i++){
+      var timeout = function(x) {
+                      setTimeout(function(){
+                        var num = song[x]
+                        numToKey[num]();
+
+                        var element = '.' + num;
+                        expandAnimation(element);
+                      }, i * speed);
+                    }
+      timeout(i);
+    }
+  }
+
   var maryHadLamb = [3,2,1,2,3,3,3,0,2,2,2,0,3,5,5,0,3,2,1,2,3,3,3,3,2,2,3,2,1];
 
-  var wreckingBall = [6,6,0,2,0,6,0,2,0,6,0,1,0,6,0,1,0,1,3,0,2,3,2,3,4,0,2,0,0,0,6,0,2,0,6,0,2,0,6,0,1,0,6,0,1,0,1,3,0,2,3,2,3,4,0,2,0];
+  var wreckingBall = [6,0,2,0,6,0,2,0,6,0,1,0,6,0,1,0,1,3,0,2,3,2,3,4,0,2,0,0,0,6,0,2,0,6,0,2,0,6,0,1,0,6,0,1,0,1,3,0,2,3,2,3,4,0,2,0];
 
   var trapQueen = [1,1,1,5,3,0,2,2,2,0,2,0,1,0,1,1,1,5,3,0,2,2,2,2,0,1,1,1];
 
   var partition = [5,0,6,0,0,7,8,0,8,7,6,5,6,7,6,0,6,7,6,0,6,7,6,0,5,0,6,0,0,7,8,0,8,7,6,5,6,7,6,0,6,7,6,0,6,7,6,0];
 
   var blankSpace = [4,4,0,4,4,0,4,4,0,4,0,6,6,5,4,5,4,5,2]
-  // plays a song that's formatted as an array of integers
-  function playSong(song, speed){
-    // sets delay while shifting through array
-    var loopSong = setInterval(function(){
-      // stop the loop once you have looped through entire array
-      if (song.length == 0){
-        clearInterval(loopSong);
-      } else {
-        // calling moveServo function
-        var num = song.shift();
-        numToKey[num]();
-
-        // do the animation
-        var element = '.' + num.toString();
-        expandAnimation(element);
-      }
-    }, speed);
-  }
 
   $('.mary').on('click', function(){
     playSong(maryHadLamb, 500);
   })
 
   $('.miley').on('click', function(){
-    playSong(wreckingBall, 400);
+    playSong(wreckingBall, 375);
   })
 
   $('.fetty').on('click', function(){
@@ -108,7 +107,7 @@ $(function(){
   })
 
   $('.beyonce').on('click', function(){
-    playSong(partition);
+    playSong(partition, 400);
   })
 
   $('.taylor').on('click', function(){
