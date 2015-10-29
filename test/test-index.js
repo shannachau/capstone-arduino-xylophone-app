03f8a7b0f738667d2ee2625aa6539e5cb67b94e1
Shanna Chau <shannachau@gmail.com>
2015-10-29 10:11:54 -0700

    Basic tests for rendering views

--- test/test-index.js
+++ test/test-index.js
@@ -1,1 +1,34 @@
-var assert = require('assert');
+var assert  = require('assert'),
+    request = require('supertest'),
+    app     = require('../app'),
+    agent   = request.agent(app);
+
+describe("routes", function(){
+  describe("GET '/'", function(){
+    it("renders index.jade", function(done){
+      agent.get('/').expect(200, function(err, res){
+        assert.equal(err, undefined);
+        done();
+      })
+    })
+  })
+
+  describe("GET '/tutorial'", function(){
+    it("renders tutorial.jade", function(done){
+      agent.get('/tutorial').expect(200, function(err, res){
+        assert.equal(err, undefined);
+        done();
+      })
+    })
+  })
+
+  describe("GET '/about'", function(){
+    it("renders index.jade", function(done){
+      agent.get('/about').expect(200, function(err, res){
+        assert.equal(err, undefined);
+        done();
+      })
+    })
+  })
+
+})
